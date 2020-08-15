d9b24f67e2b83ccb3e46918a4aecdca1dc20ebdf
drhost4 <57153741+drhost4@users.noreply.github.com>
2020-08-14 19:38:38 -0700

    Update RestaurantAPI.kt

--- src/main/java/com/example/doordashlite/api/RestaurantAPI.kt
+++ src/main/java/com/example/doordashlite/api/RestaurantAPI.kt
@@ -7,12 +7,4 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RestaurantAPI {
-    @GET("/v2/restaurant/?")
-    fun retrieveRestaurants(
-        @Query("lat") lat: Double,
-        @Query("lng") long: Double
-    ): Call<List<Restaurant>>
-
-    @GET("/v2/restaurant/{restaurant_id}/")
-    fun retrieveRestaurantDetails(@Path("restaurant_id") restaurantId: Long): Call<Restaurant>
 }
